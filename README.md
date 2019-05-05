4c043f924402d6fa0753d5df4bb3ef1895b7a973
S Anand <root.node@gmail.com>
2019-05-05 15:34:22 +0530

    ENH: add initial working version

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tambola
+
+This is a [Tambola](https://tambola.octro.com/HowToPlay/index.php) game aid,
+meant to help [conduct Tambola games at events](https://youtu.be/VgXkvXaccEY).
+
+The Caller opens the [Tambola app](index.html). This generates a random Tambola
+board that is covered.
+
+TODO: screenshot
+
+(The title is editable, and can be set to anything.)
+
+There are 3 buttons on the page. Each has an associated shortcut key.
+
+- Next (N/Space): Shows the next number
+- Show (S): Shows selected numbers so far. This helps verify winners
+- Hide (H): Hides selected numbers displayed by SHOW
---
+++ tambola.js
@@ -0,0 +1,41 @@
+let q = document.querySelector.bind(document)
+
+// Initialize a random board
+let seq = _.sortBy(_.range(0, 10 * 9), Math.random)
+let tmpl = _.template(q('.template').innerHTML)
+q('.board').innerHTML = tmpl({seq: seq})
+
+// next() shows the next number
+let $number = q('.number')
+let $numberval = q('.number .value')
+let index = 0
+let fade
+let next = () => {
+  index++
+  $numberval.innerHTML = seq[index] + 1
+  q('.cell-' + seq[index]).classList.add('marked')
+  $number.classList.add('fadein')
+  if (fade)
+    fade = clearTimeout(fade)
+  fade = setTimeout(() => $number.classList.remove('fadein'), 2000)
+}
+
+// "Show" button shows all marked cells. "Hide" button hides them
+let show_all = () => q('.board').classList.add('open')
+let hide_all = () => q('.board').classList.remove('open')
+q('.show').addEventListener('click', show_all, false)
+q('.hide').addEventListener('click', hide_all, false)
+q('.next').addEventListener('click', next, false)
+
+// Handle keyboard events to do the same
+q('body').addEventListener('keypress', (e) => {
+  if (e.target.contentEditable == 'true') return
+  else if (e.key == 's') show_all()
+  else if (e.key == 'h') hide_all()
+  else if (e.key == 'n' || e.key == ' ') next()
+})
+
+window.addEventListener('beforeunload', (e) => {
+  e.preventDefault()
+  e.returnValue = ''
+})
